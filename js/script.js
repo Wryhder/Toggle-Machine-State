cbb92756ab80922e59a08c7ef65657682f14a8d5
Wryhder <jemimahsummers@yahoo.com>
2020-02-17 11:06:43 +0100

    Control button can now start and stop animation of solar system; Clean up unused variables and redundant comments

--- js/script.js
+++ js/script.js
@@ -1,47 +1,37 @@
-$(window).load(function () {
+document.addEventListener("DOMContentLoaded", () => {
 
-    var container = $("#universe-container"),
-        universe = $("#universe"),
-        solarsys = $("#solar-system");
-
-    var init = function () {
-        container.removeClass('opening').addClass("view-3D").delay(2000).queue(function () {
-            $(this).addClass("set-speed");
-            $(this).dequeue();
-        });
-    };
-
-    // var setView = function (view) { universe.removeClass().addClass(view); };
-
-    // $(".set-view").click(function () { body.toggleClass("view-3D view-2D"); });
-    // $(".set-zoom").click(function () { body.toggleClass("zoom-large zoom-close"); });
-    // $(".set-speed").click(function () { setView("scale-stretched set-speed"); });
-    // $(".set-size").click(function () { setView("scale-s set-size"); });
-    // $(".set-distance").click(function () { setView("scale-d set-distance"); });
-
-    init();
-
-
-    const btn = document.querySelector(".off");
+    const container = $("#universe-container");
+    const btn = document.querySelector(".on");
 
     const MACHINE_STATE = {
         0: "Machine is off",
         1: "Machine is on"
     };
 
+    const init = function () {
+        container.removeClass('opening').addClass("view-3D").delay(2000).queue(function () {
+            $(this).addClass("set-speed");
+            $(this).dequeue();
+        });
+    };
+
     function toggleButtonText(text) {
         btn.textContent = text;
     }
+
+    init();
 
     btn.addEventListener('click', function () {
         switch (btn.textContent) {
             case MACHINE_STATE[0]:
                 toggleButtonText(MACHINE_STATE[1]);
                 btn.classList.replace("off", "on");
+                container.toggleClass("paused-animation");
                 break;
             case MACHINE_STATE[1]:
                 toggleButtonText(MACHINE_STATE[0]);
                 btn.classList.replace("on", "off");
+                container.toggleClass("paused-animation");
                 break;
         }
     });
