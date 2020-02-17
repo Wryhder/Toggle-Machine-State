4e3c84aeff2aacaa17930383189c5cef6604578a
Wryhder <jemimahsummers@yahoo.com>
2020-02-17 10:27:58 +0100

    Integrate a 3D solar system background
    Credit for solar system: Julian Garnier (https://codepen.io/juliangarnier/pen/idhuG)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
+$(window).load(function () {
 
-const btn = document.querySelector(".off");
+    var container = $("#universe-container"),
+        universe = $("#universe"),
+        solarsys = $("#solar-system");
 
-const MACHINE_STATE = {
-    0: "Machine is off",
-    1: "Machine is on"
-};
+    var init = function () {
+        container.removeClass('opening').addClass("view-3D").delay(2000).queue(function () {
+            $(this).addClass("set-speed");
+            $(this).dequeue();
+        });
+    };
 
-function toggleButtonText(text) {
-    btn.textContent = text;
-}
+    // var setView = function (view) { universe.removeClass().addClass(view); };
 
-btn.addEventListener('click', function () {
-    switch (btn.textContent) {
-        case MACHINE_STATE[0]:
-            toggleButtonText(MACHINE_STATE[1]);
-            btn.classList.replace("off", "on");
-            break;
-        case MACHINE_STATE[1]:
-            toggleButtonText(MACHINE_STATE[0]);
-            btn.classList.replace("on", "off");
-            break;
+    // $(".set-view").click(function () { body.toggleClass("view-3D view-2D"); });
+    // $(".set-zoom").click(function () { body.toggleClass("zoom-large zoom-close"); });
+    // $(".set-speed").click(function () { setView("scale-stretched set-speed"); });
+    // $(".set-size").click(function () { setView("scale-s set-size"); });
+    // $(".set-distance").click(function () { setView("scale-d set-distance"); });
+
+    init();
+
+
+    const btn = document.querySelector(".off");
+
+    const MACHINE_STATE = {
+        0: "Machine is off",
+        1: "Machine is on"
+    };
+
+    function toggleButtonText(text) {
+        btn.textContent = text;
     }
+
+    btn.addEventListener('click', function () {
+        switch (btn.textContent) {
+            case MACHINE_STATE[0]:
+                toggleButtonText(MACHINE_STATE[1]);
+                btn.classList.replace("off", "on");
+                break;
+            case MACHINE_STATE[1]:
+                toggleButtonText(MACHINE_STATE[0]);
+                btn.classList.replace("on", "off");
+                break;
+        }
+    });
 });
